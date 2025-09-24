53e0240a92fedd64ad0d2b9f73797c095da524c3
lightswarm124 <lightswarm@gmail.com>
2025-09-23 22:48:47 -0400

    issue with build version. need to remove bar at top of every screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 // src/main.tsx
 import './polyfills/node-globals';
-import 'dotenv/config';
+// IMPORTANT: don't import 'dotenv/config' in the browser bundle.
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
@@ -15,9 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
